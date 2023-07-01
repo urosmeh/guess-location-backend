565d963450af764ca5d77e790015e374892ea372
Uroš Meh <uros.meh@pro-bit.si>
2023-07-01 12:27:46 +0200

    unit tests for auth service

--- src/modules/auth/auth.service.spec.ts
+++ src/modules/auth/auth.service.spec.ts
@@ -1,18 +1,63 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from '../../entities/user.entity';
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let authService: AuthService;
+  let findOne: jest.Mock;
+  let save: jest.Mock;
 
   beforeEach(async () => {
+    findOne = jest.fn();
+    save = jest.fn();
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: { findOne, save },
+        },
+        {
+          provide: JwtService,
+          useValue: {
+            sign: jest.fn().mockReturnValue('jwt'),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    authService = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(authService).toBeDefined();
+  });
+
+  it('should throw an error if username is already taken', async () => {
+    findOne.mockResolvedValue(new User());
+    await expect(
+      authService.register('existingUsername', '123456'),
+    ).rejects.toThrow();
+  });
+
+  it('should return an user on successful registration', async () => {
+    const mockUser = new User();
+    mockUser.username = 'newUser';
+    mockUser.password = '123456';
+
+    findOne.mockResolvedValue(undefined);
+    save.mockResolvedValue({ username: 'newUser', password: '123456' });
+
+    const user = await authService.register('newUser', '123456');
+
+    expect(user).toBeDefined();
+    expect(user.username).toEqual(mockUser.username);
+  });
+
+  it('should return an error if username too short', async () => {
+    await expect(authService.register('asd', '123')).rejects.toThrow();
   });
 });
