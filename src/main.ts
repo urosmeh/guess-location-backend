5184fda2444fcbe72846ca724f909b37f5c4d652
Uroš Meh <uros.meh@pro-bit.si>
2023-07-01 11:07:04 +0200

    added swagger docs config and auth api docs

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 const initValidation = (app: INestApplication) =>
   app.useGlobalPipes(
@@ -11,6 +12,19 @@ const initValidation = (app: INestApplication) =>
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Guess location')
+    .setDescription(
+      'Guess location allows users to add an image and exact pin to......',
+    )
+    .setVersion('1.0')
+    .addTag('location')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   initValidation(app);
   await app.listen(3000);
 }
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,11 +2,14 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserLoginDto } from './dto/user-login.dto';
 import { UserRegisterDto } from './dto/user-register.dto';
+import { ApiBody, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiBody({ type: UserLoginDto })
   @Post('login')
   async login(@Body() dto: UserLoginDto) {
     const user = await this.authService.validateUser(
@@ -16,6 +19,7 @@ export class AuthController {
     return this.authService.login(user);
   }
 
+  @ApiBody({ type: UserRegisterDto })
   @Post('register')
   async register(@Body() dto: UserRegisterDto) {
     return this.authService.register(dto.username, dto.password);
